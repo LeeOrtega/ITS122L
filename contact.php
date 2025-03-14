ccbc1f6e8fc6a3b463772b6226ffe9fb169e5e7e
LeeOrtega <132351362+LeeOrtega@users.noreply.github.com>
2025-03-14 22:52:29 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,12 +5,11 @@ include 'config.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Check if all required fields exist
-    $f_name = isset($_POST['f_name']) ? trim($_POST['f_name']) : null;
-    $l_name = isset($_POST['l_name']) ? trim($_POST['l_name']) : null;
-    $email = isset($_POST['email']) ? trim($_POST['email']) : null;
-    $phone = isset($_POST['phone']) ? trim($_POST['phone']) : null;
-    $message = isset($_POST['message']) ? trim($_POST['message']) : null;
-
+$f_name = $_POST['f_name'];
+$l_name = $_POST['l_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
 
 
